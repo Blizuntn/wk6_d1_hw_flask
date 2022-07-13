12ba814dc003694a16e3dddf93e9831e454bffc5
Michael Bunton <mbnt84@gmail.com>
2022-07-12 22:50:55 -0500

    Addint the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
